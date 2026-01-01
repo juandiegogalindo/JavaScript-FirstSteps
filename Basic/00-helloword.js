6d182afe19ac66cc4cd7af8513b259fa48e5048b
Juan Diego Galindo <jdgalindo6@gmail.com>
2026-01-01 18:46:59 -0500

    Add initial JavaScript files with basic console logging and variable examples

---
+++ Basic/00-helloword.js
@@ -0,0 +1,14 @@
+// Comentarios
+/* Nada raro, todo igual que en Java */
+console.log("Hola Mundo JavaScript, Visual Studio Code")
+
+numero = 5+5
+console.log(numero)
+
+variable = "Juan Diego Galindo Montenegro"
+console.log(variable)
+
+algoritmo = 5 + (5*(15-5/5)+10)
+console.log(algoritmo)
+
+console.log(numero, algoritmo, variable)
